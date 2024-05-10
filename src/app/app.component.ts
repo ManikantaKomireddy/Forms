f5805a60604e75a68edb5f3612416b4ecb2d9dde
Manikanta Komireddy <manikomireddy@gmail.com>
2024-05-10 22:07:50 +0530

    Second Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';   
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Forms';
+  show(r:any){
+    console.log(r.value);
+    r.reset();
+  }
 }
